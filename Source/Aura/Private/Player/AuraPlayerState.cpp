81ef882c13a32aab8a07a121493943d0c4847157
lusholeal <lucholeal7788@gmail.com>
2023-10-15 20:57:16 -0500

    The Player State

---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,9 @@
+// Copyright for LushoLeal Projects
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	NetUpdateFrequency = 100.f; //how often the server tries to update clients
+}
