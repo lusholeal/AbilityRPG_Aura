c29a9576cbd77b6baa76bea33bda940568a42c7e
lusholeal <lucholeal7788@gmail.com>
2023-11-06 13:31:32 -0500

    Duration Gameplay Effects

--- Intermediate/Build/Win64/UnrealEditor/Inc/Aura/UHT/Aura.init.gen.cpp
+++ Intermediate/Build/Win64/UnrealEditor/Inc/Aura/UHT/Aura.init.gen.cpp
@@ -27,7 +27,7 @@ void EmptyLinkFunctionForGeneratedCodeAura_init() {}
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0xB6F40EC7,
+				0xC59EFCDF,
 				0xBB3BD608,
 				METADATA_PARAMS(nullptr, 0)
 			};
@@ -35,5 +35,5 @@ void EmptyLinkFunctionForGeneratedCodeAura_init() {}
 		}
 		return Z_Registration_Info_UPackage__Script_Aura.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Aura(Z_Construct_UPackage__Script_Aura, TEXT("/Script/Aura"), Z_Registration_Info_UPackage__Script_Aura, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xB6F40EC7, 0xBB3BD608));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Aura(Z_Construct_UPackage__Script_Aura, TEXT("/Script/Aura"), Z_Registration_Info_UPackage__Script_Aura, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xC59EFCDF, 0xBB3BD608));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
