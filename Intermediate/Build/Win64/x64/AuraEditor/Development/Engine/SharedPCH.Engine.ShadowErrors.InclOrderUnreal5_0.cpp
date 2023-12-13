1c24fa24b9336bee393da9d6fb10ccbadabe2643
lusholeal <lucholeal7788@gmail.com>
2023-12-13 16:05:32 -0500

    Message Widget

---
+++ Intermediate/Build/Win64/x64/AuraEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.InclOrderUnreal5_0.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.34.31947
+#include "C:/Users/Luisl/OneDrive/Escritorio/AuraRPG/Aura/Intermediate/Build/Win64/x64/AuraEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.InclOrderUnreal5_0.h"
